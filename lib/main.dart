4e4789b23b72b1bbe057b22056b3c04999ea7390
joaoVelosoT <vitorvteixeira2023@gmail.com>
2024-10-30 16:36:38 -0300

    fazendo post para api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:aula_http_flutter/views/cadastro.dart';
+import 'package:aula_http_flutter/views/cadastroSucesso.dart';
 import 'package:aula_http_flutter/views/homepage.dart';
+import 'package:aula_http_flutter/views/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Homepage(),
+      home: const Login(),
     );
   }
 }
